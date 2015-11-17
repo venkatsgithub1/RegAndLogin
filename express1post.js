66d991ac0bdaf2fe323223fe68676b17af2e3875
venkatsgithub1 <venkatsgithub@gmail.com>
2015-11-17 23:22:15 +0530

    Update express1post.js

--- express1post.js
+++ express1post.js
@@ -15,7 +15,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "password",
+	password: "somePass",
 	database: "mysql"
 });
 
